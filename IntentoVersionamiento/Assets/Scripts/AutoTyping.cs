547f3a3d2d0396e3f2386e8606c9277aaa04558c
herakratz <juan.balcazar@3dves.com>
2015-12-23 13:19:01 -0500

    añadido de unos modelos

---
+++ IntentoVersionamiento/Assets/Scripts/AutoTyping.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
+[RequireComponent (typeof(Text))]
+
+public class AutoTyping : MonoBehaviour {
+
+    public string[] Mensaje = new string[1]; 
+    public float retrasoInicial = 2f; 
+    public float retrasoIntermedio = 0.01f; 
+    public AudioClip sonido; 
+    Text t; 
+    int a = 0;
+
+    void Start() 
+    {
+        StartCoroutine("Comenzar");
+    }
+
+    void Awake() 
+    {
+        t = GetComponent<Text>();
+    }
+
+    public IEnumerator Comenzar() 
+    {
+        for (int f = 0; f < Mensaje.Length; f++)
+        {
+            a = f;
+            StartCoroutine("Escribir"); 
+            yield return new WaitForSeconds(retrasoIntermedio*Mensaje[a].Length*2 + retrasoInicial*1.5f); 
+        }
+    }
+    public IEnumerator Escribir()  
+    {
+        yield return new WaitForSeconds(retrasoInicial);
+        for (int i = 0; i < Mensaje[a].Length+1 ; i++)  
+        {
+            t.text = Mensaje[a].Substring(0, i); 
+
+            if(sonido != null)
+                GetComponent<AudioSource>().PlayOneShot(sonido); 
+            yield return new WaitForSeconds(retrasoIntermedio); 
+         }
+    }
+
+}
+
---
+++ IntentoVersionamiento/Assets/Scripts/Jugabilidad/EventosPausa.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventosPausa : MonoBehaviour {
+
+	bool paused = false;
+	bool back = false;
+	GameObject panemenu;
+	GameObject doorToken;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		Cursor.visible = false;
+		Screen.lockCursor = true;
+		//GameObject.Find ("XboxControl").GetComponent<XboxMouseControl> ().enabled = false;
+		doorToken = GameObject.Find ("doortokens");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			paused = togglePaused ();
+		}
+		//if (Input.GetButtonDown ("360_Start")) {
+		//	paused = togglePaused ();
+		//}
+	
+	}
+
+	bool togglePaused ()
+	{
+		if (paused) {
+			if ( Application.loadedLevelName == "Main") {
+				GameObject.Find ("Player").GetComponent<FirstControl> ().enabled = true;
+				GameObject.Find ("Player").GetComponent<Mouselook> ().enabled = true;
+				//GameObject.Find ("Player").GetComponent<Select> ().enabled = true;
+				//GameObject.Find ("InputHandler").GetComponent<MostrarEtiquetas> ().enabled = true;
+				//GameObject.Find ("InputHandler").GetComponent<cameraSwitch> ().enabled = true;
+				//GameObject.Find ("camVistaTop").GetComponent<moveAxisCam> ().enabled = true;
+				//GameObject.Find ("flyCam").GetComponent<moveAxisFlyCam> ().enabled = true;
+				doorToken.SetActive (true);
+			}
+			//GameObject.Find ("XboxControl").GetComponent<XboxMouseControl> ().enabled = false;
+			//panemenu.SetActive (false);
+			if (!back) {
+				Cursor.visible = false;
+				Screen.lockCursor = true;				
+			}
+			return(false);
+		} else {
+			if ( Application.loadedLevelName == "Main") {
+				GameObject.Find ("Player").GetComponent<FirstControl> ().enabled = false;
+				GameObject.Find ("Player").GetComponent<Mouselook> ().enabled = false;
+				//GameObject.Find ("Player").GetComponent<Select> ().enabled = false;
+				//GameObject.Find ("InputHandler").GetComponent<MostrarEtiquetas> ().enabled = false;
+				//GameObject.Find ("InputHandler").GetComponent<cameraSwitch> ().enabled = false;
+				//GameObject.Find ("camVistaTop").GetComponent<moveAxisCam> ().enabled = false;
+				//GameObject.Find ("flyCam").GetComponent<moveAxisFlyCam> ().enabled = false;
+				doorToken.SetActive (false);
+			}
+			//GameObject.Find ("XboxControl").GetComponent<XboxMouseControl> ().enabled = true;
+			//panemenu.SetActive (true);
+			Cursor.visible = true;
+			Screen.lockCursor = false;
+			return(true);
+		}
+	}
+}
